78d5ab0f7aeca0f0a02bd2750c685b38eaaf31e0
yoxu515 <71214332+yoxu515@users.noreply.github.com>
2023-06-27 19:49:04 +0800

    Update vop_eval.sh

--- vop_eval.sh
+++ vop_eval.sh
@@ -1,8 +1,2 @@
-# python vop_eval.py --data_path './VIPSeg_O1/valid' --res_path '/Users/yoxu/Documents/Exp/ms_r50_v' --eval_decay
-# python vop_eval.py --data_path './VIPSeg_O1/valid' --res_path '/Users/yoxu/Documents/Exp/ms_r50_yd' --eval_decay
-# python vop_eval.py --data_path './VIPSeg_O1/valid' --res_path '/Users/yoxu/Documents/Exp/ms_r50_ydv' --eval_decay
-# python vop_eval.py --data_path './VIPSeg_O1/valid' --res_path '/Users/yoxu/Documents/Exp/ms_swinb_v' --eval_decay
-# python vop_eval.py --data_path './VIPSeg_O1/valid' --res_path '/Users/yoxu/Documents/Exp/ms_swinb_yd' --eval_decay
-python vop_eval.py --data_path './VIPSeg_O1/valid' --res_path '/Users/yoxu/Documents/Exp/ms_swinb_ydv' --eval_decay
-python vop_eval.py --data_path './VIPSeg_O1/valid' --res_path '/Users/yoxu/Documents/Exp/pano_r50' --eval_decay
-python vop_eval.py --data_path './VIPSeg_O1/valid' --res_path '/Users/yoxu/Documents/Exp/pano_swinb' --eval_decay
+python vop_eval.py --data_path $PATH_TO_VIPOSEG_VALID --res_path $PATH_TO_RESULT --eval_decay
+
